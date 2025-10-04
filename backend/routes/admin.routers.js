d3026eb39fc721e2dc1d0889e052e876f8f7d684
pmanh24 <manhphung2405@gmail.com>
2025-10-04 12:20:58 +0700

    search-filter-changeStatus

--- backend/routes/admin.routers.js
+++ backend/routes/admin.routers.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { listAccounts } = require("../controller/listAccount.admin.controller");
+const { listAccounts, updateAccountStatus } = require("../controller/listAccount.admin.controller");
 
 router.get("/admin/accounts", listAccounts);
+router.patch("/admin/accounts/:accountId", updateAccountStatus);
 
 module.exports = router;
