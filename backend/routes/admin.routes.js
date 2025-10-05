aae6fa0d08246b4a17e1bb7e32f5677a644c7125
pmanh24 <manhphung2405@gmail.com>
2025-10-05 17:18:55 +0700

    Auth&api(front-end)

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { listAccounts, updateAccountStatus } = require("../controller/listAccount.admin.controller");
+const authenticate = require('../middleware/authenticate.middleware')
+const authorize = require('../middleware/authorize.middleware')
 
-router.get("/admin/accounts", listAccounts);
-router.patch("/admin/accounts/:accountId", updateAccountStatus);
+router.get("/admin/accounts",authenticate,authorize("ADMIN"), listAccounts);
+router.patch("/admin/accounts/:accountId",authenticate,authorize("ADMIN"), updateAccountStatus);
 
 module.exports = router;
--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { upload } = require("../middleware/uploadImage.middleware");
 const { getProfile, updateProfile } = require("../controller/updateProfile.controller");
+const authenticate = require('../middleware/authenticate.middleware')
 
-router.get("/auth/profile/:account_id", getProfile);
+const checkOwner = (req, res, next) => {
+  const { accountId } = req.user;
+  const targetAccountId = req.params.account_id;
+
+  if (accountId !== targetAccountId) {
+    return res.status(403).json({ message: "No access" });
+  }
+  next();
+};
+
+router.get("/auth/profile/:account_id", authenticate,checkOwner, getProfile);
 //router.patch("/auth/profile/:account_id", upload.single("avatar"), updateProfile);
-router.patch("/auth/profile/:account_id", updateProfile);
+router.patch("/auth/profile/:account_id",authenticate,checkOwner, updateProfile);
 
 module.exports = router;
